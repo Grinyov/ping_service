65d48dbcded0155c57885bf4b17296552f6fd75a
vgrinyov <vitaliy.grinyov@gmail.com>
2017-02-28 17:14:27 +0200

    add ws controller

--- src/main/java/com/grinyov/pingutil/web/PingWSController.java
+++ src/main/java/com/grinyov/pingutil/web/PingWSController.java
@@ -1,5 +1,10 @@
 package com.grinyov.pingutil.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 
 /**
@@ -8,5 +13,18 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class PingWSController {
 
+    @Autowired
+    SimpMessagingTemplate template;
 
+//    @Scheduled(fixedDelay = 20000L)
+//    @SendTo("/topic/pings)
+//    public sendPong() {
+//        template.convertAndSend("/topic/pings", "pong (periodic)");
+//    }
+
+    @MessageMapping("/pings")
+    @SendTo("/topic/pings")
+    public String sendPingResponse() {
+        return "pong (response)";
+    }
 }
